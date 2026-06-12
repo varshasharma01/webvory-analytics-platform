465e785552c31dc56d7dac7194cd2ff494b52a07
Varsha <95166671+varshasharma01@users.noreply.github.com>
2026-06-12 15:11:38 +0530

    Delete TalentTrend directory

--- TalentTrend/db/connection.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient # pyright: ignore[reportMissingImports]
-
-def get_db():
-    client = MongoClient('mongodb+srv://varshasinbox1_db_user:Varsha%401700@cluster0.9f396vf.mongodb.net/')
-    db = client['TalentTrend']
-    print("Connected to MongoDB")
-    return db
-
-get_db()
